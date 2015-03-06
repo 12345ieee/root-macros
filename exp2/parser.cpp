3fd530c451b98cf823db348bfdd27518cf2f42ec
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-03-06 13:56:35 +0100

    Nearly finished event parsing

--- exp2/parser.cpp
+++ exp2/parser.cpp
@@ -12,6 +12,17 @@
 
 using namespace std;
 
+class hit
+{
+	double x;
+	double y;
+	
+	public:
+	
+	hit(double _x, double _y): x(_x), y(_y) {}
+};
+
+
 class eeevent
 {
 	int num1;
@@ -25,17 +36,36 @@ class eeevent
 	int clk1;
 	int clk2;
 	
-	vector<int[2]> event1;
-	vector<int[2]> event2;
-	vector<int[2]> event3;
+	vector<hit> event1;
+	vector<hit> event2;
+	vector<hit> event3;
 	
 	double hv1;
 	double hv2;
 	double hv3;
 	
-	eeevent(int num1, int num_trig, int num_acq, int secs, int nsecs, int clk1, int clk2, int hv1, int hv2, int hv3):
+	public:
+	
+	eeevent(int num1, int num_trig, int num_acq, int secs, int nsecs, int clk1, int clk2, double hv1, double hv2, double hv3):
 		num1(num1), num_trig(num_trig), num_acq(num_acq), secs(secs), nsecs(nsecs),
 		clk1(clk1), clk2(clk2), hv1(hv1), hv2(hv2), hv3(hv3) {}
+	
+	void gethit(double x, double y, int z) {
+		hit newhit(x, y);
+		switch (z) {
+			case 149:
+				this->event1.push_back(newhit);
+				break;
+			case 95:
+				this->event2.push_back(newhit);
+				break;
+			case 49:
+				this->event3.push_back(newhit);
+				break;
+			default:
+				cout << "Unrecognized chamber" << endl;
+		}
+	}
 };
 
 int parse(string path)
@@ -49,7 +79,6 @@ int parse(string path)
 	
 	vector<eeevent> evector;               // vector of events
 	double hv1, hv2, hv3;                  // variables for high voltage values
-	
 	
 	while (!file.eof()) {
 		file.getline(line, linesize);       // fill line
@@ -66,16 +95,44 @@ int parse(string path)
 			char* next=piece+7;              // hack to get after "STATUS"
 			sscanf(next, "HV %lf %*f %lf %*f %lf %*f", &hv1, &hv2, &hv3); 
 			cout << "hv: " << hv1 << " - " << hv2 << " - " << hv3 << endl;
-			continue;
 		}
 		else if (descriptor=="EVENT") {
-			//cout << "e" << endl;
-		}
-		else continue;
-		
-		while (piece != NULL) {
-			piece = strtok (NULL, " \t");    // get more pieces
-			//cout << piece << endl;
+			
+			piece = strtok (NULL, " \t");    // get num_trig
+			int num_trig = atoi(piece);
+			
+			piece = strtok (NULL, " \t");    // get num_acq
+			int num_acq = atoi(piece);
+			
+			piece = strtok (NULL, " \t");    // get seconds
+			int secs = atoi(piece);
+			
+			piece = strtok (NULL, " \t");    // get nseconds
+			int nsecs = atoi(piece);
+			
+			piece = strtok (NULL, " \t");    // get clk1
+			int clk1 = atoi(piece);
+			
+			piece = strtok (NULL, " \t");    // get clk2
+			int clk2 = atoi(piece);
+			
+			eeevent neweeevent(num1, num_trig, num_acq, secs, nsecs, clk1, clk2, hv1, hv2, hv3);
+			
+			piece = strtok (NULL, " \t");    // get events
+			while (piece != NULL) {
+				double x = atof(piece);
+				piece = strtok (NULL, " \t");    // get more pieces
+				
+				double y = atof(piece);
+				piece = strtok (NULL, " \t");    // get more pieces
+				
+				int z = atoi(piece);
+				piece = strtok (NULL, " \t");    // get more pieces
+				
+				neweeevent.gethit(x, y, z);
+			}
+			
+			evector.push_back(neweeevent);
 		}
 	}
 
