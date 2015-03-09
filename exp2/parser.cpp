9eb3ff189efb4312323bf7f9ca56e34d8661796a
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-03-09 07:11:12 +0100

    Completed efficiency part

--- exp2/parser.cpp
+++ exp2/parser.cpp
@@ -14,12 +14,12 @@ using namespace std;
 
 class hit
 {
+	public:
+	
 	double x;
 	double y;
 	
-	public:
-	
-	vector<hit> cluster;
+	//vector<hit> cluster;
 	
 	hit(double x, double y): x(x), y(y) {}
 	
@@ -28,6 +28,10 @@ class hit
 		double Dx2 = pow((this->x - h.x), 2);
 		double Dy2 = pow((this->y - h.y), 2);
 		return sqrt(Dx2 + Dy2);
+	}
+	
+	bool is_inside(double xmax, double ymax) {
+		return (abs(x)<=xmax && abs(y)<=ymax);
 	}
 };
 
@@ -151,26 +155,28 @@ vector<eeevent> parse(string path)
 			evector.push_back(neweeevent);
 		}
 	}
-	cout << "Parsing complete: " << evector.size() << " events captured" << endl;
+	cout << "Parsing completed: " << evector.size() << " events captured" << endl << endl;
 	
 	return evector;
 }
 
-void cluster_size_study(eeevector evector, double step, double maxradius)
+void cluster_size_study(eeevector evector)
 {
-	// Create the canvas
-	TCanvas *canv = new TCanvas("canvas","canvas title",800,600);
-	canv->cd();
+	const double step = 0.01;
+	const double maxradius = 20;
 	
+	// Create the canvas
+	TCanvas *canv = new TCanvas("canvas","canvas title", 800, 600);
+	canv->cd();
 	TH1D *hist = new TH1D("", "Cluster", maxradius/step-1, 0, maxradius);
 	
 	for (size_t i=0; i<evector.size(); ++i) {              // for every event
 		eeevent event1 = evector[i];
 		for (int ch=0; ch<3; ++ch) {                        // for every chamber
-			if (event1.hits[ch].size() > 1) {                      // if there are hits on ch
+			if (event1.hits[ch].size() > 1) {                      // if there are hits in ch
 				for (size_t j=0; j<event1.hits[ch].size(); ++j) {   // for every hit in ch
 					hit hit1 = event1.hits[ch][j];
-					for (size_t k=j+1; k<event1.hits[ch].size(); ++k) {   // for other hit in ch
+					for (size_t k=j+1; k<event1.hits[ch].size(); ++k) {   // for every other hit in ch
 						hit hit2 = event1.hits[ch][k];
 						double dist = hit2.distance_from(hit1);
 						for (double radius=maxradius; radius > dist; radius-=step) {   // for every radius
@@ -181,21 +187,94 @@ void cluster_size_study(eeevector evector, double step, double maxradius)
 			}
 		}
 	}
+	hist->SetStats(kFALSE);
 	hist->Draw();
-	cout << "Done" << endl;
+	cout << "Done clusters" << endl << endl;
+}
+
+void chamber_assign(int chtest, int* ch1, int* ch2)
+{
+	switch (chtest) {
+		case 0: *ch1 = 1; *ch2 = 2; break;
+		case 1: *ch1 = 0; *ch2 = 2; break;
+		case 2: *ch1 = 0; *ch2 = 1; break;
+		default: cout << "Unrecognized chamber" << endl;
+	}
+}
+
+hit compute_hitpoint(int chtest, hit hit1, hit hit2)
+{
+	double z, z1, z2;
+	switch (chtest) {
+		case 0: z = 149.00; z1 =  95.50; z2 =  49.00; break;
+		case 1: z =  95.50; z1 = 149.00; z2 =  49.00; break;
+		case 2: z =  49.00; z1 = 149.00; z2 =  95.50; break;
+		default: cout << "Unrecognized chamber" << endl; exit(1);
+	}
+	// line equation is: (x-x1)/(x2-x1)=(y-y1)/(y2-y1)=(z-z1)/(z2-z1)=A
+	
+	double x1 = hit1.x;
+	double x2 = hit2.x;
+	double y1 = hit1.y;
+	double y2 = hit2.y;
+	
+	double A = (z-z1)/(z2-z1);
+	double x = (x2-x1)*A +x1;
+	double y = (y2-y1)*A +y1;
+	return {x, y};
+} 
+
+void compute_eff(int chtest, eeevector evector)
+{
+	const double xmax=2000;
+	const double ymax=2000;
+	const double dist_cutoff=10;
+	
+	int expected=0;
+	int detected=0;
+	
+	int ch1, ch2;
+	chamber_assign(chtest, &ch1, &ch2);
+	
+	for (size_t i=0; i<evector.size(); ++i) {              // for every event
+		eeevent event1 = evector[i];
+		for (size_t j=0; j<event1.hits[ch1].size(); ++j) {   // for every hit in ch1
+			hit hit1 = event1.hits[ch1][j];
+			for (size_t k=0; k<event1.hits[ch2].size(); ++k) {   // for every hit in ch2
+				hit hit2 = event1.hits[ch2][k];
+				hit hit_test = compute_hitpoint(chtest, hit1, hit2);
+				if (hit_test.is_inside(xmax, ymax)) {
+					expected++;
+					for (size_t h=0; h<event1.hits[chtest].size(); ++h) {   // for every hit in chtest
+						hit hitf = event1.hits[chtest][h];
+						double dist = hit_test.distance_from(hitf);
+						if (dist < dist_cutoff) {
+							detected++;
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+	cout << "Distance cutoff chosen: " << dist_cutoff << endl;
+	cout << "Expected events: " << expected << endl;
+	cout << "Detected events: " << detected << endl;
+	cout << "Efficiency: " << (double)detected/expected << endl << endl;
+	
 }
 
 int parser(string path)
 {
-	const double step = 0.01;
-	const double maxradius = 20;
 	
 	eeevector evector;
 	evector = parse(path);
 	
-	cluster_size_study(evector, step, maxradius);
+
+	//cluster_size_study(evector);
+	//evector = clusterize(evector, cluster_size);
 	
-	//compute_eff(1, evector);
+	compute_eff(1, evector);
 	
 	return 0;
 }
