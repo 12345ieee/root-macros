021ea61f2f9bc04b887776d9aa94ba5871803fdb
Andrea Stacchiotti <andrea_stacchiotti@hotmail.it>
2015-05-28 21:08:15 +0200

    Stared the second part, I can plot the data, woohoo

--- neutrino_mass/nmass.cpp
+++ neutrino_mass/nmass.cpp
@@ -63,4 +63,47 @@ void gun(string filename="gun.dat")
 	cout << endl << "Resolution = (" << resolution << " \\pm " << errResolution << ") meV" << endl;
 }
 
-
+void nmass(string filename="kurie.dat")
+{
+	// res from before: (10.009 \pm 0.008) meV
+	double sigma = 0.01; // in eV, keep it simple
+	
+	const double emin = 18.5504;  // min of file
+	const double emax = 18.6176;  // max of file
+	const double estep=  0.0008;  // step for file
+	const int    nbins= (emax-emin)/estep + estep;  // should be 85
+	const double pmin = emin-estep/2;  // min of plot 18.55
+	const double pmax = emax+estep/2;  // max of plot 18.618
+	
+	ifstream file;
+	file.open(filename.c_str());
+	if (!file) {
+		cout << "File does not exist" << endl;
+		return;
+	}
+	
+	TCanvas* canv = new TCanvas("ckurie");
+	TH1D* hk = new TH1D("hk", "Kurie plot", nbins, pmin, pmax);
+	
+	double energy;
+	int nevents;
+	
+	while (!file.eof()) {
+		file >> energy >> nevents;
+		//cout << energy << " - "<< nevents << endl;
+		for (int i=0; i<nevents; ++i)
+			hk->Fill(energy);
+	}
+	
+	/*TF1* f = new TF1("3gauss", "gaus(0)+gaus(3)+gaus(6)", emin, emax);
+	f->SetParameters(
+		13300, 18.55, 0.01,
+		17700, 18.60, 0.01,
+		 8900, 18.65, 0.01);
+	
+	TFitResultPtr pResult = hk->Fit(f, "S"); // "S" gets a hold of the result data
+	*/
+	canv->cd();
+	canv->SetLogy();
+	hk->Draw("PE");
+}
