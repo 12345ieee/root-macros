86abe04518dabb571ee5aed4d6c0a42cf754a9eb
Andrea Stacchiotti <andreastacchiotti@gmail.com>
2016-01-27 18:34:08 +0100

    Added template macro

---
+++ macro_ROOT.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+// #include <fstream>
+// #include <cmath>
+// #include <ctime>
+
+// #include "TH1.h"
+// #include "TH2.h"
+// #include "TH3.h"
+// #include "TCanvas.h"
+// #include "TRandom3.h"
+// #include "TFile.h"
+// #include "TGraph.h"
+// #include "TString.h"
+
+using namespace std;
+
+int macro()
+{
+	
+	return 0;
+}
+
+int main(int, char**)
+{
+	return macro();
+}
